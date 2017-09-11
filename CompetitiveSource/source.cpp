32bf9dbc7b9845e71a89e492747910679f41ce4a
Windsority <mcfengxin@gmail.com>
2017-09-11 13:52:12 +0800

    Base code

---
+++ CompetitiveSource/source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <cstdlib>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+#define INF 1000000000
+
+int main()
+{
+	
+}
