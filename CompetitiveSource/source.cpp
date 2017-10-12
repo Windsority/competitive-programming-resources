5cdcfec07fe922c8a964ca86e3fe17c314b63ef8
Windsority <mcfengxin@gmail.com>
2017-10-12 16:49:55 +0800

    Update source.cpp

--- CompetitiveSource/source.cpp
+++ CompetitiveSource/source.cpp
@@ -6,13 +6,15 @@
 #include <cstdlib>
 #include <cmath>
 #include <vector>
+#include <queue>
 using namespace std;
 
 typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<int> vi;
+typedef vector<ii> vii;
 #define INF 1000000000
-#define LL_INF 10000000000000000LL
+#define MINF 10000000000000000LL
 
 int main()
 {
